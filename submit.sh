9601cdc6ba7c77944a61e8c8a79853e333d711ef
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-04-14 15:36:33 +0200

    adjust submit.sh

--- submit.sh
+++ submit.sh
@@ -2,51 +2,64 @@
 
 set -exo pipefail
 
-jobName="OpenEO-GeoPySpark"
+source ./env.sh
+
+image=${HADOOP_IMAGE:-"vito-docker-private.artifactory.vgt.vito.be/python38-hadoop:latest"}
+
 pysparkPython="venv/bin/python"
-
-export HDP_VERSION=3.1.0.0-78
+export HDP_VERSION=3.1.4.0-315
 export SPARK_MAJOR_VERSION=2
-export SPARK_HOME=/usr/hdp/${HDP_VERSION}/spark2
-export LD_LIBRARY_PATH="venv/lib64"
-
-export PYTHONPATH="venv/lib64/python3.6/site-packages:venv/lib/python3.6/site-packages"
-
-hdfsVenvZip=https://artifactory.vgt.vito.be/auxdata-public/openeo/venv36.zip
-extensions=https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.2.0-SNAPSHOT/geotrellis-extensions-2.2.0-SNAPSHOT.jar
-backend_assembly=https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.7-openeo.jar
-
-echo "Found backend assembly: ${backend_assembly}"
+export PYTHONPATH="venv/lib64/python3.8/site-packages:venv/lib/python3.8/site-packages"
+export SPARK_HOME=/opt/spark3_2_0
+export PATH="$SPARK_HOME/bin:$PATH"
 
 
-spark-submit \
- --master yarn --deploy-mode cluster \
- --queue default \
- --name ${jobName} \
- --principal mep_tsviewer@VGT.VITO.BE --keytab mep_tsviewer.keytab \
- --driver-memory 10G \
- --executor-memory 5G \
- --conf spark.driver.memoryOverhead=3g \
- --conf spark.executor.memoryOverhead=2g \
- --conf spark.driver.maxResultSize=2g \
- --conf spark.speculation=true \
- --conf spark.speculation.quantile=0.4 --conf spark.speculation.multiplier=1.1 \
- --conf spark.dynamicAllocation.minExecutors=5 --conf spark.dynamicAllocation.maxExecutors=30 \
- --conf spark.locality.wait=300ms --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true \
- --conf spark.yarn.appMasterEnv.PYTHON_EGG_CACHE=./ \
- --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=${pysparkPython} \
- --conf spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON=${pysparkPython} \
- --conf spark.executorEnv.PYSPARK_PYTHON=${pysparkPython} \
- --conf spark.executorEnv.LD_LIBRARY_PATH=venv/lib64:/tmp_epod/gdal:venv/lib64/python3.6/site-packages/jep \
- --conf spark.yarn.appMasterEnv.LD_LIBRARY_PATH=venv/lib64:/tmp_epod/gdal \
- --conf spark.executorEnv.PROJ_LIB=/tmp_epod/gdal/data \
- --conf spark.yarn.appMasterEnv.PROJ_LIB=/tmp_epod/gdal/data \
- --conf spark.yarn.appMasterEnv.OPENEO_VENV_ZIP=${hdfsVenvZip} \
- --conf spark.yarn.appMasterEnv.WMTS_BASE_URL_PATTERN=http://openeo.vgt.vito.be/openeo/services/%s \
- --conf spark.executorEnv.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --conf spark.yarn.appMasterEnv.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
- --conf spark.executorEnv.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} --conf spark.yarn.appMasterEnv.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
- --files layercatalog.json \
- --archives "${hdfsVenvZip}#venv" \
- --conf spark.hadoop.security.authentication=kerberos --conf spark.yarn.maxAppAttempts=1 \
- --jars ${extensions},${backend_assembly} \
- openeogeotrellis/deploy/probav-mep.py
+spark_job_name=Catboost_on_cluster
+queue="openeo"
+minExecutors=4
+driverCores=1
+
+yarn_runtime='docker'
+sparkDriverJavaOptions="-Dscala.concurrent.context.numThreads=6"
+sparkExecutorJavaOptions="-Dscala.concurrent.context.numThreads=6"
+
+echo "Deploying with Docker: ${DOCKER}"
+
+${SPARK_HOME}/bin/spark-submit \
+   --master yarn --deploy-mode cluster \
+   --queue ${queue} \
+   --name ${spark_job_name} \
+   --principal openeo@VGT.VITO.BE --keytab openeo.keytab \
+   --driver-memory 10G \
+   --executor-memory 3G \
+   --executor-cores 2 \
+   --driver-cores ${driverCores} \
+   --driver-java-options "${sparkDriverJavaOptions}" \
+   --conf "spark.executor.extraJavaOptions=${sparkExecutorJavaOptions}" \
+   --conf spark.yarn.submit.waitAppCompletion=false \
+   --conf spark.driver.memoryOverhead=7g \
+   --conf spark.executor.memoryOverhead=2g \
+   --conf spark.driver.maxResultSize=2g \
+   --conf spark.kryoserializer.buffer.max=1G \
+   --conf spark.scheduler.mode="FAIR" \
+   --conf spark.yarn.appMasterEnv.PYTHON_EGG_CACHE=./ \
+   --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=${pysparkPython} \
+   --conf spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON=${pysparkPython} \
+   --conf "spark.yarn.appMasterEnv.JAVA_HOME=/usr/lib/jvm/jre-11-openjdk" \
+   --conf "spark.executorEnv.JAVA_HOME=/usr/lib/jvm/jre-11-openjdk" \
+   --conf spark.executorEnv.PYSPARK_PYTHON=${pysparkPython} \
+   --conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS=/var/lib/sss/pubconf/krb5.include.d:/var/lib/sss/pubconf/krb5.include.d:ro,/var/lib/sss/pipes:/var/lib/sss/pipes:rw,/usr/hdp/current/:/usr/hdp/current/:ro,/etc/hadoop/conf/:/etc/hadoop/conf/:ro,/etc/krb5.conf:/etc/krb5.conf:ro,/etc/ipa/:/etc/ipa/:ro,/data/users:/data/users:rw,/tmp_epod/openeo_collecting:/tmp_epod/openeo_collecting:rw,/tmp_epod/openeo_assembled:/tmp_epod/openeo_assembled:rw \
+   --conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_TYPE=${yarn_runtime} \
+   --conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=${image}  \
+   --conf spark.executorEnv.YARN_CONTAINER_RUNTIME_TYPE=${yarn_runtime} \
+   --conf spark.executorEnv.YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=${image}  \
+   --conf spark.executorEnv.YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS=/var/lib/sss/pubconf/krb5.include.d:/var/lib/sss/pubconf/krb5.include.d:ro,/var/lib/sss/pipes:/var/lib/sss/pipes:rw,/usr/hdp/current/:/usr/hdp/current/:ro,/etc/hadoop/conf/:/etc/hadoop/conf/:ro,/etc/krb5.conf:/etc/krb5.conf:ro,/data/MTDA:/data/MTDA:ro,/data/MEP:/data/MEP:ro,/data/users:/data/users:rw,/data/projects/OpenEO:/data/projects/OpenEO:rw \
+   --conf spark.ui.view.acls.groups=vito \
+   --conf spark.modify.acls.groups=vito \
+   --conf spark.extraListeners="org.openeo.sparklisteners.CancelRunawayJobListener" \
+   --conf spark.hadoop.yarn.timeline-service.enabled=false \
+   --conf spark.hadoop.yarn.client.failover-proxy-provider=org.apache.hadoop.yarn.client.ConfiguredRMFailoverProxyProvider \
+   --conf spark.shuffle.service.name=spark_shuffle_320 --conf spark.shuffle.service.port=7557 \
+   --archives "openeo-venv38-${ZIP_VERSION}.zip#venv" \
+   --conf spark.hadoop.security.authentication=kerberos --conf spark.yarn.maxAppAttempts=1 \
+   catboost_on_cluster.py
