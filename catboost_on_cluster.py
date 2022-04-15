32792aad5f3bbfbfe72c7ae670c1ec60302862e0
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-04-15 11:09:24 +0200

    use catboost_spark instead of catboost module

--- catboost_on_cluster.py
+++ catboost_on_cluster.py
@@ -1,41 +1,68 @@
-from geopyspark import geopyspark_conf
-from pyspark import SparkContext, SparkConf
+from pyspark.sql import Row, SparkSession
+from pyspark.ml.linalg import Vectors, VectorUDT
+from pyspark.sql.types import *
 
-conf = (geopyspark_conf(master="local[*]", appName="catboost")
-        .set("spark.jars.packages", "ai.catboost:catboost-spark_3.1_2.12:1.0.4")
-        )
-SparkContext(conf=conf)
+spark = (SparkSession.builder
+  .master("local[*]")
+  .config("spark.jars.packages", "ai.catboost:catboost-spark_3.1_2.12:1.0.4")
+  .appName("ClassifierTest")
+  .getOrCreate()
+)
+
 import catboost_spark
 
-import numpy as np
+srcDataSchema = [
+    StructField("features", VectorUDT()),
+    StructField("label", StringType())
+]
 
-from catboost import CatBoostClassifier, Pool
+trainData = [
+    Row(Vectors.dense(0.1, 0.2, 0.11), "1"),
+    Row(Vectors.dense(0.97, 0.82, 0.33), "2"),
+    Row(Vectors.dense(0.13, 0.22, 0.23), "1"),
+    Row(Vectors.dense(0.8, 0.62, 0.0), "0")
+]
 
-# initialize data
-train_data = np.random.randint(0,
-                               100,
-                               size=(100, 10))
+trainDf = spark.createDataFrame(spark.sparkContext.parallelize(trainData), StructType(srcDataSchema))
+trainPool = catboost_spark.Pool(trainDf)
 
-train_labels = np.random.randint(0,
-                                 2,
-                                 size=(100))
+evalData = [
+    Row(Vectors.dense(0.22, 0.33, 0.9), "2"),
+    Row(Vectors.dense(0.11, 0.1, 0.21), "0"),
+    Row(Vectors.dense(0.77, 0.0, 0.0), "1")
+]
 
-test_data = catboost_pool = Pool(train_data,
-                                 train_labels)
+evalDf = spark.createDataFrame(spark.sparkContext.parallelize(evalData), StructType(srcDataSchema))
+evalPool = catboost_spark.Pool(evalDf)
 
-model = CatBoostClassifier(iterations=2,
-                           depth=2,
-                           learning_rate=1,
-                           loss_function='Logloss',
-                           verbose=True)
-# train the model
-model.fit(train_data, train_labels)
-# make the prediction using the resulting model
-preds_class = model.predict(test_data)
-preds_proba = model.predict_proba(test_data)
-print("class = ", preds_class)
-print("proba = ", preds_proba)
+classifier = catboost_spark.CatBoostClassifier()
 
+# train a model
+model = classifier.fit(trainPool, [evalPool])
 
+# apply the model
+predictions = model.transform(evalPool.data)
+predictions.show()
 
+# save the model
+savedModelPath = "/my_models/multiclass_model"
+model.write().save(savedModelPath)
+
+# save the model as a local file in CatBoost native format
+savedNativeModelPath = './my_local_models/multiclass_model.cbm'
+model.saveNativeModel(savedNativeModelPath)
+
+# load the model (can be used in a different Spark session)
+
+loadedModel = catboost_spark.CatBoostClassificationModel.load(savedModelPath)
+
+predictionsFromLoadedModel = loadedModel.transform(evalPool.data)
+predictionsFromLoadedModel.show()
+
+# load the model as a local file in CatBoost native format
+
+loadedNativeModel = catboost_spark.CatBoostClassificationModel.loadNativeModel(savedNativeModelPath)
+
+predictionsFromLoadedNativeModel = loadedNativeModel.transform(evalPool.data)
+predictionsFromLoadedNativeModel.show()
 
