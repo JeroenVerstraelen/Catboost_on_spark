0847b2755adc3069a5e7465b8c3a25c614bb01d1
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-05-06 15:15:22 +0200

    rename upload_tiles_to_artifactory.py

--- upload_tiles_to_artifactory.py
+++ upload_tiles_to_artifactory.py
@@ -4,8 +4,8 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--username", help="User name", type=str, required=True)
 parser.add_argument("-p", "--password", help="Password", type=str, required=True)
-parser.add_argument("-d", "--directory", help="Directory to upload", type=str, required=True)
-parser.add_argument("-a", "--adir", help="Artifactory directory name, defaults to eu_croptype", type=str, default="eu_croptype")
+parser.add_argument("-d", "--directory", help="Directory to upload.", type=str, required=True)
+parser.add_argument("-a", "--adir", help="Artifactory directory name, defaults to eu_croptype.", type=str, default="eu_croptype")
 
 
 def main():
